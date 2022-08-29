91dab325cfc78bc3ba41a48f2b31b386fcc3c9ef
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-29 14:02:57 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let index = 0; index < array.length; index++){
+    const results = target - array[index]
+    const restItems = array.slice(index+1)
+    if(restItems.includes(results)){
+      return true
+    }
+
+  }
+  return false
 }
 
+console.log(hasTargetSum([1, 2, 5], 4))
 /* 
   Write the Big O time complexity of your function here
+  our runtime will be quadratic
+
 */
 
 /* 
   Add your pseudocode here
+  iterate through the elements
+  take an element and deduct from target save the results
+  check if results exist in the rest of elements
+  if it does return true, false otherwise 
 */
 
 /*
